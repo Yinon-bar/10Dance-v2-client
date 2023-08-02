96ad7a778c007b39f220c46342bbf6eb4ec8bf7c
Yinon Bar <yinonbar1988@gmail.com>
2023-08-02 21:59:54 +0300

    admin screen table

--- src/Components/Pages/Admin/Admin.jsx
+++ src/Components/Pages/Admin/Admin.jsx
@@ -1,13 +1,48 @@
+import axios from "axios";
 import HeaderAdmin from "../../Header/HeaderAdmin/HeaderAdmin";
 import "./Admin.css";
+import { useEffect, useState } from "react";
+import AttTable from "./AttTable/AttTable";
 
 function Admin() {
+    const [attendee, setAttendee] = useState([]);
+
+    const handleEvent = (e) => {
+        console.log(e.target.value);
+        axios
+            .get("http://localhost:3001/api/" + e.target.value, {
+                headers: { "Content-Type": "application/json" },
+            })
+            .then((resp) => {
+                console.log(resp.data);
+                setAttendee(resp.data);
+            })
+            .catch((err) => console.log(err));
+    };
+
+    useEffect(() => {
+        // להוסיף לפה קריאה של כל הטבלאות שיש בדאטהבייס
+    }, []);
+
     return (
         <div className="Admin">
             <HeaderAdmin />
             <div className="container">
                 <div className="content">
                     <h1>ברוכים הבאים לממשק הניהול</h1>
+                    <h3>בחר אירוע להצגה</h3>
+                    <select
+                        onChange={(e) => handleEvent(e)}
+                        name="בחר אירוע להצגה"
+                        id=""
+                    >
+                        <option defaultChecked hidden value="">
+                            ללא
+                        </option>
+                        <option value="present-day">יום עיון</option>
+                        <option value="attendees">כנס גיאוגרפיה</option>
+                    </select>
+                    <AttTable attendee={attendee} />
                 </div>
             </div>
         </div>
---
+++ src/Components/Pages/Admin/AttTable/AttTable.jsx
@@ -0,0 +1,35 @@
+import "./AttTable.css";
+
+function AttTable(props) {
+    console.log(props.attendee);
+    return (
+        <div className="AttTable">
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>תעודת זהות</th>
+                        <th>שם פרטי</th>
+                        <th>שם משפחה</th>
+                        <th>מוסד לימודים</th>
+                        <th>נוכח</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.attendee.map((att) => (
+                        <tr key={att.id}>
+                            <td>{att.id}</td>
+                            <td>{att.tz_id}</td>
+                            <td>{att.fName}</td>
+                            <td>{att.lName}</td>
+                            <td>{att.institute}</td>
+                            <td>{att.isArrived}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default AttTable;
