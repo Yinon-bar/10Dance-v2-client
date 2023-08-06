c3e8e518235672f89dc49c4f1e18d2a8346a2e42
Yinon Bar <yinonbar1988@gmail.com>
2023-08-06 21:28:26 +0300

    build

--- src/Components/NumPad/ListNumpad/ListNumpad.jsx
+++ src/Components/NumPad/ListNumpad/ListNumpad.jsx
@@ -3,16 +3,16 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { BackspaceBtn } from "../SingleNumpad/BackspaceBtn/BackspaceBtn";
 import { ClearallBtn } from "../SingleNumpad/ClearallBtn/ClearallBtn";
-import SingleNumpad from "../SingleNumpad/SingleNumpad";
+// import SingleNumpad from "../SingleNumpad/SingleNumpad";
 
 function ListNumpad() {
-    const num = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+    // const num = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
     return (
         <div className="ListNumpad">
-            {num.map((num) => (
+            {/* {num.map((num) => (
                 <SingleNumpad key={num} num={num} />
-            ))}
+            ))} */}
             <BackspaceBtn />
             <ClearallBtn />
             <button className="btn-submit">
