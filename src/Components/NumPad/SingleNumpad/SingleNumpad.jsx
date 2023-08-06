555a6686bdc6a4191b94308591bdd024b2825b05
Yinon Bar <yinonbar1988@gmail.com>
2023-08-06 21:33:03 +0300

    build

--- src/Components/NumPad/SingleNumpad/SingleNumpad.jsx
+++ src/Components/NumPad/SingleNumpad/SingleNumpad.jsx
@@ -1,20 +1,20 @@
-// import { useContext } from "react";
-// import "./singleNumpad.css";
-// import NumberContext from "../../../Context/NumberContext";
+import { useContext } from "react";
+import "./singleNumpad.css";
+import NumberContext from "../../../Context/NumberContext";
 
-// function SingleNumpad(props) {
-//     const { inputValue, setInputValue } = useContext(NumberContext);
+function SingleNumpad(props) {
+    const { inputValue, setInputValue } = useContext(NumberContext);
 
-//     return (
-//         <div className="SingleNumPad">
-//             <div
-//                 className="btn"
-//                 onClick={(e) => setInputValue(inputValue + e.target.innerHTML)}
-//             >
-//                 {props.num}
-//             </div>
-//         </div>
-//     );
-// }
+    return (
+        <div className="SingleNumPad">
+            <div
+                className="btn"
+                onClick={(e) => setInputValue(inputValue + e.target.innerHTML)}
+            >
+                {props.num}
+            </div>
+        </div>
+    );
+}
 
-// export default SingleNumpad;
+export default SingleNumpad;
