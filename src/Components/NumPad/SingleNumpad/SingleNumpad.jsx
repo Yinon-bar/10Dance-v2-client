80de437408d3d0c59aacb8628b7a072582633ac1
Yinon Bar <yinonbar1988@gmail.com>
2023-08-06 21:34:27 +0300

    build

--- src/Components/NumPad/SingleNumpad/SingleNumpad.jsx
+++ src/Components/NumPad/SingleNumpad/SingleNumpad.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import "./singleNumpad.css";
+import "./SingleNumpad.css";
 import NumberContext from "../../../Context/NumberContext";
 
 function SingleNumpad(props) {
