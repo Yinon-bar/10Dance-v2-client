9ba55dc786729e4b8090eb040dd9d401743e8bfa
ינון בר <yinonb@hebrew-academy.org.il>
2023-08-10 11:18:10 +0300

    db config

--- src/Components/Pages/Admin/Admin.jsx
+++ src/Components/Pages/Admin/Admin.jsx
@@ -38,18 +38,18 @@ function Admin() {
         axios
             .get(
                 // Local
-                "http://localhost:3001/api/tables"
-                // "https://one0dance-v2-nodejs-mysql.onrender.com/api/tables"
+                // "http://localhost:3001/api/tables",
+                "https://one0dance-v2-nodejs-mysql.onrender.com/api/tables",
                 // סתאם לנסיון
                 // "https://jsonplaceholder.typicode.com/todos",
-                // {
-                //     headers: {
-                //         "Content-Type": "application/json",
-                //         "Access-Control-Allow-Origin": "*",
-                //         "Access-Control-Allow-Methods":
-                //             "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-                //     },
-                // }
+                {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Methods":
+                            "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+                    },
+                }
             )
             .then((resp) => {
                 console.log(resp.data);
