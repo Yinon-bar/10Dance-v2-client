7394753e9671cf5a8be9c18a1a3cb09bb321a2b8
Yinon Bar <yinonbar1988@gmail.com>
2024-04-02 20:34:15 +0300

    old

--- src/Components/Pages/Admin/Admin.jsx
+++ src/Components/Pages/Admin/Admin.jsx
@@ -8,110 +8,109 @@ import { useContext } from "react";
 import CurrentTableContext from "../../../Context/CurrentTableContext";
 
 function Admin() {
-    const [attendee, setAttendee] = useState([]);
-    const [tables, setTables] = useState([]);
-    const [addAttendee, setAddAttendee] = useState(false);
-    let { currentTable, setCurrentTable } = useContext(CurrentTableContext);
+  const [attendee, setAttendee] = useState([]);
+  const [tables, setTables] = useState([]);
+  const [addAttendee, setAddAttendee] = useState(false);
+  let { currentTable, setCurrentTable } = useContext(CurrentTableContext);
 
-    const handleEvent = (e) => {
-        // console.log(e.target.value);
-        axios
-            .get(
-                // Local
-                // "http://localhost:3001/api/" +
-                // Render hosting
-                "https://one0dance-v2-nodejs-mysql.onrender.com/api/" +
-                    e.target.value
-            )
-            .then((resp) => {
-                // console.log(resp.data);
-                setAttendee(resp.data);
-                setCurrentTable(resp.data);
-            })
-            .catch((err) => console.log(err));
-    };
-    console.log(currentTable);
+  const handleEvent = (e) => {
+    // console.log(e.target.value);
+    axios
+      .get(
+        // Local
+        "http://localhost:3001/api/" +
+          // Render hosting
+          // "https://one0dance-v2-nodejs-mysql.onrender.com/api/" +
+          e.target.value
+      )
+      .then((resp) => {
+        // console.log(resp.data);
+        setAttendee(resp.data);
+        setCurrentTable(resp.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  console.log(currentTable);
 
-    const handleSearch = (term) => {
-        console.log(term);
-    };
+  const handleSearch = (term) => {
+    console.log(term);
+  };
 
-    useEffect(() => {
-        // להוסיף לפה קריאה של כל הטבלאות שיש בדאטהבייס
-        axios
-            .get(
-                // Local
-                // "http://localhost:3001/api/tables",
-                "https://one0dance-v2-nodejs-mysql.onrender.com/api/tables"
-                // סתאם לנסיון
-                // "https://jsonplaceholder.typicode.com/todos",
-                // {
-                //     headers: {
-                //         "Content-Type": "application/json",
-                //         "Access-Control-Allow-Origin": "*",
-                //         "Access-Control-Allow-Methods":
-                //             "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-                //     },
-                // }
-            )
-            .then((resp) => {
-                console.log(resp.data);
-                setTables(resp.data);
-            })
-            .catch((err) => console.log(err));
-    }, []);
+  useEffect(() => {
+    // להוסיף לפה קריאה של כל הטבלאות שיש בדאטהבייס
+    axios
+      .get(
+        // Local
+        // "http://localhost:3001/api/tables",
 
-    return (
-        <div className="Admin">
-            <HeaderAdmin />
-            <div className="container">
-                <div className="content">
-                    <h1>ברוכים הבאים לממשק הניהול</h1>
-                    <div className="commandRibbon">
-                        <div className="rightSide">
-                            <h3>בחר אירוע להצגה</h3>
-                            <select
-                                onChange={(e) => handleEvent(e)}
-                                name="בחר אירוע להצגה"
-                                id=""
-                            >
-                                <option defaultChecked hidden value="">
-                                    ללא
-                                </option>
-                                {tables.map((table, index) => (
-                                    <option
-                                        key={index}
-                                        value={table.Tables_in_zerdance_general}
-                                    >
-                                        {table.Tables_in_zerdance_general}
-                                    </option>
-                                ))}
-                            </select>
-                        </div>
-                        <div className="leftSide">
-                            <input
-                                className="searchAtt"
-                                type="text"
-                                onChange={(e) => handleSearch(e.target.value)}
-                                placeholder="חיפוש אנשים"
-                            />
-                            <button
-                                className="btn btn-primary"
-                                onClick={() => setAddAttendee(true)}
-                            >
-                                +
-                            </button>
-                            <button className="btn btn-primary printScreen">
-                                הדפס
-                            </button>
-                        </div>
-                    </div>
-                    <AttTable attendee={attendee} />
-                </div>
+        // זה הנכוןןן
+        // "https://one0dance-v2-nodejs-mysql.onrender.com/api/tables"
+
+        "http://localhost/10Dance-V2-php-server/API/attendees/read.php"
+        // סתאם לנסיון
+        // "https://jsonplaceholder.typicode.com/todos",
+        // {
+        //     headers: {
+        //         "Content-Type": "application/json",
+        //         "Access-Control-Allow-Origin": "*",
+        //         "Access-Control-Allow-Methods":
+        //             "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+        //     },
+        // }
+      )
+      .then((resp) => {
+        console.log(resp.data);
+        setTables(resp.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="Admin">
+      <HeaderAdmin />
+      <div className="container">
+        <div className="content">
+          <h1>ברוכים הבאים לממשק הניהול</h1>
+          <div className="commandRibbon">
+            <div className="rightSide">
+              <h3>בחר אירוע להצגה</h3>
+              <select
+                onChange={(e) => handleEvent(e)}
+                name="בחר אירוע להצגה"
+                id=""
+              >
+                <option defaultChecked hidden value="">
+                  ללא
+                </option>
+                {tables.map((table, index) => (
+                  <option key={index} value={table.Tables_in_zerdance_general}>
+                    {table.Tables_in_zerdance_general}
+                  </option>
+                ))}
+              </select>
             </div>
-            {addAttendee && <AddModal closeModal={(e) => setAddAttendee(e)} />}
+            <div className="leftSide">
+              <input
+                className="searchAtt"
+                type="text"
+                onChange={(e) => handleSearch(e.target.value)}
+                placeholder="חיפוש אנשים"
+              />
+              <button
+                className="btn btn-primary"
+                onClick={() => setAddAttendee(true)}
+              >
+                +
+              </button>
+              <button className="btn btn-primary printScreen">הדפס</button>
+            </div>
+          </div>
+          <AttTable attendee={attendee} />
         </div>
-    );
+      </div>
+      {addAttendee && <AddModal closeModal={(e) => setAddAttendee(e)} />}
+    </div>
+  );
 }
 
 export default Admin;
