d0b3a47be7e9ea6f3ad99ef693058db2eddfad8a
Yinon Bar <yinonbar1988@gmail.com>
2024-04-04 23:21:13 +0300

    selecting correct event

--- src/Components/Pages/Welcome/Welcome.jsx
+++ src/Components/Pages/Welcome/Welcome.jsx
@@ -3,25 +3,33 @@ import "./Welcome.css";
 import Header from "../../Header/Header";
 
 function Welcome() {
-    return (
-        <div className="Welcome">
-            <Header />
-            <div className="container">
-                <div className="content">
-                    <h1>
-                        ברוכים הבאים לתכנת{" "}
-                        <span className="cng-font">10Dance</span>
-                    </h1>
-                    <NavLink className="btn btn-primary" to={"admin"}>
-                        כניסה למסך ניהול
-                    </NavLink>
-                    <NavLink className="btn btn-primary" to={"client"}>
-                        למסך קבלת פנים
-                    </NavLink>
-                </div>
-            </div>
+  return (
+    <div className="Welcome">
+      <Header />
+      <div className="container">
+        <div className="content">
+          <h1>
+            ברוכים הבאים לתכנת <span className="cng-font">10Dance</span>
+          </h1>
+          <h2>אנא הכנס פרטי התחברות</h2>
+          <form action="">
+            <label>
+              שם משתמש:
+              <input type="text" name="" />
+            </label>
+            <br />
+            <label>
+              סיסמה:
+              <input type="text" name="" />
+            </label>
+          </form>
+          <NavLink className="btn btn-primary" to={"admin"}>
+            כניסה
+          </NavLink>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Welcome;
