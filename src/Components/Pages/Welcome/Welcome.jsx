6faa88cdeeacb3fb65a019e930b49cc0fe0880df
Yinon Bar <yinonbar1988@gmail.com>
2024-04-06 18:44:43 +0300

    selecting correct event

--- src/Components/Pages/Welcome/Welcome.jsx
+++ src/Components/Pages/Welcome/Welcome.jsx
@@ -1,8 +1,41 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import axios from "axios";
 import "./Welcome.css";
 import Header from "../../Header/Header";
+import { useState } from "react";
 
 function Welcome() {
+  const navigate = useNavigate();
+  const [msg, setMsg] = useState(false);
+  const [user, setUser] = useState({
+    user_email: "",
+    user_password: "",
+    jwt: "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJsb2NhbGhvc3QiLCJpYXQiOjE3MTI0MDQ2NDYsIm5iZiI6MTcxMjQwNDY0NiwiZXhwIjoxNzEyNjYzODQ2LCJhdWQiOiJteXVzZXJzIiwiZGF0YSI6eyJpZCI6MSwibmFtZSI6Inlpbm9uX2JhciIsImVtYWlsIjoieWlub25iYXIxOTg4QGdtYWlsLmNvbSJ9fQ.Ew0pBePym9b_PQwLpDB69BXROtraOMTclSZTvBPWlkk",
+  });
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log(user);
+    axios
+      .post(
+        "http://localhost/10Dance-V2-php-server/4-controllers/login.php",
+        user
+      )
+      .then((resp) => {
+        console.log(resp);
+        if (resp.data.error) {
+          // console.log(resp.data.error);
+          setMsg(true);
+        } else {
+          // console.log(resp.data);
+          navigate("admin");
+        }
+      })
+      .catch((error) => {
+        // console.log(error);
+      });
+  };
+
   return (
     <div className="Welcome">
       <Header />
@@ -12,20 +45,34 @@ function Welcome() {
             ברוכים הבאים לתכנת <span className="cng-font">10Dance</span>
           </h1>
           <h2>אנא הכנס פרטי התחברות</h2>
-          <form action="">
+          {msg && <h2>פרטי החיבור אינם נכונים</h2>}
+          <form action="" className="login">
             <label>
-              שם משתמש:
-              <input type="text" name="" />
+              אימייל
+              <input
+                type="text"
+                onChange={(e) =>
+                  setUser({ ...user, user_email: e.target.value })
+                }
+              />
             </label>
             <br />
             <label>
               סיסמה:
-              <input type="text" name="" />
+              <input
+                type="text"
+                onChange={(e) =>
+                  setUser({ ...user, user_password: e.target.value })
+                }
+              />
             </label>
+            <button onClick={(e) => handleLogin(e)} className="btn btn-primary">
+              כניסה
+            </button>
           </form>
-          <NavLink className="btn btn-primary" to={"admin"}>
+          {/* <NavLink className="btn btn-primary" to={"admin"}>
             כניסה
-          </NavLink>
+          </NavLink> */}
         </div>
       </div>
     </div>
