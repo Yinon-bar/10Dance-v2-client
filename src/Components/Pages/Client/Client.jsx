506f66c5d7c2192520b3d2f02dee29013cfec1d0
inonab <yinonbar1988@gmail.com>
2025-12-07 23:26:22 +0200

    Client fixes

--- src/Components/Pages/Client/Client.jsx
+++ src/Components/Pages/Client/Client.jsx
@@ -7,87 +7,87 @@ import ListNumpad from "../../NumPad/ListNumpad/ListNumpad";
 import Header from "../../Header/Header";
 
 function Client() {
-    const [inputValue, setInputValue] = useState("");
-    const [msg, setMsg] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [printAtt, setPrintAtt] = useState(false);
-    const [attendee, setAttendee] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const [msg, setMsg] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [printAtt, setPrintAtt] = useState(false);
+  const [attendee, setAttendee] = useState([]);
 
-    const showMsg = (arg, status = 1) => {
-        setMsg(arg);
-        setTimeout(() => {
-            setMsg(null);
-            status == 1 && setPrintAtt(true);
-            setInputValue("");
-        }, 3000);
-    };
+  const showMsg = (arg, status = 1) => {
+    setMsg(arg);
+    setTimeout(() => {
+      setMsg(null);
+      status == 1 && setPrintAtt(true);
+      setInputValue("");
+    }, 3000);
+  };
 
-    const sendId = (e) => {
-        e.preventDefault();
-        const tzId = e.target.elements.tzId.value;
-        // console.log(e.target.elements.tzId.value);
-        setLoading(true);
-        axios
-            .get("https://one0dance-v2-nodejs-mysql.onrender.com/" + tzId)
-            .then((resp) => {
-                console.log(resp.data);
-                setAttendee(resp.data);
-                setLoading(false);
-                resp.data.length > 0
-                    ? showMsg("נרשמת בהצלחה!")
-                    : showMsg("אינך רשום במערכת, אנא גש לעמדת הרישום", 0);
-            })
-            .catch((err) => console.log(err));
-    };
+  const sendId = (e) => {
+    e.preventDefault();
+    const tzId = e.target.elements.tzId.value;
+    // console.log(e.target.elements.tzId.value);
+    setLoading(true);
+    axios
+      .get(
+        "http://localhost/10Dance-V2-php-server/4-controllers/get-attendee-by-tz.php?tz=" +
+          tzId
+      )
+      .then((resp) => {
+        console.log(resp.data);
+        setAttendee(resp.data);
+        setLoading(false);
+        resp.data.length > 0
+          ? showMsg("נרשמת בהצלחה!")
+          : showMsg("אינך רשום במערכת, אנא גש לעמדת הרישום", 0);
+      })
+      .catch((err) => console.log(err));
+  };
 
-    const insertToInput = (e) => {
-        console.log(e);
-        setInputValue(e);
-    };
+  const insertToInput = (e) => {
+    console.log(e);
+    setInputValue(e);
+  };
 
-    return (
-        <NumberContext.Provider
-            value={{ inputValue: inputValue, setInputValue: setInputValue }}
-        >
-            <div className="Client">
-                <Header />
-                {msg && (
-                    <div className="loading">
-                        <h1>{msg}</h1>
-                    </div>
-                )}
-                {loading && (
-                    <div className="loading">
-                        <h1>טוען נתונים...</h1>
-                    </div>
-                )}
-                {printAtt && <PrintAtt attendee={attendee[0]} />}
-                <div className="title-section">
-                    <h1 className="title welcome">ברוכים הבאים</h1>
-                    <h1 className="title campus">
-                        יום אוריינטציה לעובדים חדשים
-                    </h1>
-                    <h1 className="title welcome">האוניברסיטה העברית</h1>
-                </div>
-                <h2 className="title cta">
-                    נא הכנס ת.ז. מלאה <span className="without">כולל</span> ספרת
-                    ביקורת
-                </h2>
-                <form onSubmit={sendId}>
-                    <input
-                        onChange={(e) => insertToInput(e.target.value)}
-                        className="input-text"
-                        type="text"
-                        value={inputValue}
-                        name="tzId"
-                    />
-                    <div className="numpad">
-                        <ListNumpad />
-                    </div>
-                </form>
-            </div>
-        </NumberContext.Provider>
-    );
+  return (
+    <NumberContext.Provider
+      value={{ inputValue: inputValue, setInputValue: setInputValue }}
+    >
+      <div className="Client">
+        <Header />
+        {msg && (
+          <div className="loading">
+            <h1>{msg}</h1>
+          </div>
+        )}
+        {loading && (
+          <div className="loading">
+            <h1>טוען נתונים...</h1>
+          </div>
+        )}
+        {printAtt && <PrintAtt attendee={attendee[0]} />}
+        <div className="title-section">
+          <h1 className="title welcome">ברוכים הבאים</h1>
+          <h1 className="title campus">יום אוריינטציה לעובדים חדשים</h1>
+          <h1 className="title welcome">האוניברסיטה העברית</h1>
+        </div>
+        <h2 className="title cta">
+          נא הכנס ת.ז. מלאה <span className="without">כולל</span> ספרת ביקורת
+        </h2>
+        <form onSubmit={sendId}>
+          <input
+            onChange={(e) => insertToInput(e.target.value)}
+            className="input-text"
+            type="text"
+            value={inputValue}
+            name="tzId"
+          />
+          <div className="numpad">
+            <ListNumpad />
+          </div>
+        </form>
+      </div>
+    </NumberContext.Provider>
+  );
 }
 
 export default Client;
