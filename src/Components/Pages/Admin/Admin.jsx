b6ecf2bd3a6d400e19a12f028517ad2370233569
Yinon Bar <yinonbar1988@gmail.com>
2025-12-08 11:56:04 +0200

    Local storage fixes

--- src/Components/Pages/Admin/Admin.jsx
+++ src/Components/Pages/Admin/Admin.jsx
@@ -11,26 +11,26 @@ import AuthContext from "../../../Context/AuthContext";
 function Admin() {
   const [attendee, setAttendee] = useState([]);
   const [tables, setTables] = useState([]);
+  const [currentTable, setCurrentTable] = useState();
   const { currentEvent, setCurrentEvent } = useContext(CurrentEvent);
-  const { userFromDb, setUserFromDb } = useContext(AuthContext);
 
   const handleEvent = (e) => {
-    console.log(e.target.value);
+    console.log(e);
     axios
       .get(
         // Local
         "http://localhost/10Dance-V2-php-server/4-controllers/get-all-attendees.php?tableName=" +
-          e.target.value
+          e
       )
       .then((resp) => {
-        // console.log(resp.data);
+        console.log(resp.data);
+        localStorage.setItem("Current Table", e);
         setAttendee(resp.data);
       })
       .catch((err) => console.log(err));
   };
 
   useEffect(() => {
-    // console.log(tables);
     axios
       .get("http://localhost/10Dance-V2-php-server/4-controllers/read.php")
       .then((resp) => {
@@ -38,6 +38,15 @@ function Admin() {
         setTables(resp.data);
       })
       .catch((err) => console.log(err));
+    if (
+      localStorage.getItem("Current Table") !== null &&
+      localStorage.getItem("Current Table").length > 0
+    ) {
+      console.log("Not null");
+      setCurrentTable(localStorage.getItem("Current Table"));
+      handleEvent(localStorage.getItem("Current Table"));
+      console.log(currentTable);
+    }
   }, []);
 
   return (
@@ -47,12 +56,19 @@ function Admin() {
         <div className="content">
           <h1>ברוכים הבאים לממשק הניהול</h1>
           <h3>בחר אירוע להצגה</h3>
-          <select onChange={(e) => handleEvent(e)} name="בחר אירוע להצגה" id="">
+          <select
+            onChange={(e) => handleEvent(e.target.value)}
+            name="בחר אירוע להצגה"
+          >
             <option defaultChecked hidden value="">
               ללא
             </option>
             {tables.map((table, index) => (
-              <option key={index} value={table.event_table}>
+              <option
+                selected={table.event_table === currentTable}
+                key={index}
+                value={table.event_table}
+              >
                 {table.event_name}
               </option>
             ))}
