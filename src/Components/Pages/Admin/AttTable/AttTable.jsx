8f26a88eb106cab7822d4add98ec8df80b72ec6f
Yinon Bar <yinonbar1988@gmail.com>
2025-12-10 12:02:56 +0200

    Delete Attendee and update table (Parent component)

--- src/Components/Pages/Admin/AttTable/AttTable.jsx
+++ src/Components/Pages/Admin/AttTable/AttTable.jsx
@@ -1,35 +1,82 @@
+import axios from "axios";
 import "./AttTable.css";
+import { FaTrashAlt } from "react-icons/fa";
+import { FaCheckCircle } from "react-icons/fa";
+import { useState } from "react";
 
 function AttTable(props) {
-    // console.log(props.attendee);
+  const [message, setMessage] = useState("");
+
+  const handleDelete = (attendeeObj) => {
+    console.log(attendeeObj);
+    removeAttendee(attendeeObj);
+  };
+
+  const removeAttendee = async (attendeeToDelte) => {
+    try {
+      const resp = await axios.delete(
+        `http://localhost/10Dance-V2-php-server/4-controllers/delete-attendee.php?table_name=dec_geo&id=${attendeeToDelte.id}`
+      );
+      console.log(resp);
+      setMessage(resp.data);
+      setTimeout(() => {
+        setMessage("");
+        props.rerenderTable(true);
+      }, 3000);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  if (message) {
     return (
-        <div className="AttTable">
-            <table>
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>תעודת זהות</th>
-                        <th>שם פרטי</th>
-                        <th>שם משפחה</th>
-                        <th>מוסד לימודים</th>
-                        <th>נוכח</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {props.attendee.map((att) => (
-                        <tr key={att.id}>
-                            <td>{att.id}</td>
-                            <td>{att.tz_id}</td>
-                            <td>{att.fName}</td>
-                            <td>{att.lName}</td>
-                            <td>{att.institute}</td>
-                            <td>{att.isArrived}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+      <div className="AttTable">
+        <div className="messageModal">
+          <h1>
+            <FaCheckCircle size={100} />
+          </h1>
+          <h1 className="deleteMessage">המשתמש נמחק בהצלחה</h1>
         </div>
+      </div>
     );
+  }
+
+  // console.log(props.attendee);
+  return (
+    <div className="AttTable">
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>מחיקה</th>
+            <th>תעודת זהות</th>
+            <th>שם פרטי</th>
+            <th>שם משפחה</th>
+            <th>מוסד לימודים</th>
+            <th>נוכח</th>
+          </tr>
+        </thead>
+        <tbody>
+          {props.attendee.map((att) => (
+            <tr key={att.id}>
+              <td>{att.id}</td>
+              <td>
+                <FaTrashAlt
+                  className="trash"
+                  onClick={() => handleDelete(att)}
+                />
+              </td>
+              <td>{att.tz_id}</td>
+              <td>{att.fName}</td>
+              <td>{att.lName}</td>
+              <td>{att.institute}</td>
+              <td>{att.isArrived}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default AttTable;
