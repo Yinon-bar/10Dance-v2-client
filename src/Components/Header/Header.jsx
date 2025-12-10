655a58af403bd47192e098deeb248e822153141d
Yinon Bar <yinonbar1988@gmail.com>
2025-12-10 16:09:14 +0200

    Client CSS fixes - logo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,10 @@
 import "./Header.css";
-import logo from "../../assets/img/לוגו גזור.png";
+import logo from "../../assets/img/10dance_logo_white.jpg";
 
 function Header() {
   return (
     <div className="Header">
-      <div className="logo-line">
-        <img className="logo logo-10dance" src={logo} alt="" />
-      </div>
+      <img className="logo-10dance" src={logo} alt="" />
     </div>
   );
 }
