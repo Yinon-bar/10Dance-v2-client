c9bf13d7746ae921d19ac863d66cb9300099b532
Yinon Bar <yinonbar1988@gmail.com>
2025-12-10 14:40:12 +0200

    new logo

--- src/Components/Pages/Welcome/Welcome.jsx
+++ src/Components/Pages/Welcome/Welcome.jsx
@@ -1,28 +1,20 @@
 import { NavLink, Outlet, useNavigate } from "react-router-dom";
-import axios from "axios";
 import "./Welcome.css";
 import Header from "../../Header/Header";
 import { useState } from "react";
-import AuthContext from "../../../Context/AuthContext";
-import { useContext } from "react";
-import Login from "./Login/Login";
 
 function Welcome() {
   const navigate = useNavigate();
   const [authUser, setAuthUser] = useState();
-  const { userFromDb, setUserFromDb } = useContext(AuthContext);
   const [msg, setMsg] = useState(false);
 
   return (
-    // <AuthContext.Provider value={{ ...userFromDb, userFromDb: authUser }}>
     <div className="Welcome">
       <Header />
       <div className="container">
-        {/* <Login /> */}
         <Outlet />
       </div>
     </div>
-    // </AuthContext.Provider>
   );
 }
 
