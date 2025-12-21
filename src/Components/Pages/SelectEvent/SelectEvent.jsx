f266332cc3ea30065e7afff3f1b63de3de55e433
inonab <yinonbar1988@gmail.com>
2025-12-22 00:38:59 +0200

    Adding screen bfore admin

--- src/Components/Pages/SelectEvent/SelectEvent.jsx
+++ src/Components/Pages/SelectEvent/SelectEvent.jsx
@@ -16,14 +16,6 @@ const SelectEvent = () => {
             <EventCard />
             <EventCard />
             <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
-            <EventCard />
           </div>
         </div>
       </div>
